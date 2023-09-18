a213565ed6558fc7eb8c3dc95ff4aa94af3f8989
Prranav <prranavbabbar2317@gmail.com>
2023-09-19 01:51:24 +0530

    daksh Dead

--- Frontend/src/components/mainpage.tsx
+++ Frontend/src/components/mainpage.tsx
@@ -86,7 +86,20 @@ const home = () => {
                         className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         aria-current="page"
                       >
-                        Login/Sign-Up
+                        Login
+                      </a>
+                    </Link>
+                  )}
+                </li>
+                <li>
+                  {!login && (
+                    <Link legacyBehavior href="/register">
+                      <a
+                        href="#"
+                        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        aria-current="page"
+                      >
+                        Sign-Up
                       </a>
                     </Link>
                   )}
