ac51ef9e6fae077c2dac05f0d74f16c4003f5c40
Yashita Bansal <Yashiabansal39@gmail.com>
2023-09-18 19:04:29 +0530

    made regsiter page

---
+++ Frontend/src/pages/register.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <>
+    <div className="bg-[url('/bg2.png')] bg-cover bg-white flex justify-center items-center h-screen w-screen border-2">
+    {/* <h2 className="text-base font-semibold leading-7 text-black">Personal Information</h2>
+      <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
+        <div className='border-2 border-gray-400 rounded-md p-8 w-1/2'>
+        <div className = "font-bold text-black text-lg flex justify-center">Personal Information</div>
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-3">
+          <label className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+          <div className="mt-2">
+            <input type="text" name="first-name" id="first-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+          <div className="mt-2">
+            <input type="text" name="last-name" id="last-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div className="mt-2">
+            <input id="email" name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div className="mt-2">
+            <input id="email" name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">Organization</label>
+          <div className="mt-2">
+            <input id="email" name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">Contact No.</label>
+          <div className="mt-2">
+            <input id="email" name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+      </div>
+      <div className="sm:col-span-4 flex justify-center items-center mt-8">
+          <button
+              className="text-white bg-black hover:black hover:scale-[1.05] focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              
+            >
+              Add Details
+            </button>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default register
